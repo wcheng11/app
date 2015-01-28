206e1c8484073fea9de227d7e189dea7f32f4b5c
xuelong.gu@sunlights.cc <xuelong.gu@sunlights.cc>
2015-01-28 12:28:57 +0800

    跳转url设置

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,12 +5,6 @@
 .constant("ENV", {
   "name": "development",
   "apiEndpoint": "http://dev.yoursite.com:10000/"
-});
+})
 
-angular.module('myApp')
-    .constant("baseUrl","http://192.168.0.97:9008" )
-    .constant("forgetUrl","/v1/customer/forgetPwd/" )
-    .constant("verifyCodeUrl","/v1/msg/verifycode" )
-    .constant("loginUrl","/v1/customer/login" )
-    .constant("getUserUrl","/v1/customer" )
-    .constant("modifyPwdUrl","/v1/password" );
+;
